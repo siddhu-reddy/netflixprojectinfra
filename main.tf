683ec7a1d66f121e8f0f49e35395e2e974f3f0bc
siddhu-reddy <121557022+siddhu-reddy@users.noreply.github.com>
2025-01-24 13:01:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+region="us-east-1"
+}
+resource "aws_instance" "one"{
+  count=4
+  ami="ami-043a5a82b6cf98947"
+  instance_type="t2.medium"
+  vpc_security_groups_ids="sg-003550d9d3300d12f"
+  tags{
+  name=var.instance_name[count.index]
+  }
+}
+variable "instance_name" {
+  default=["jenkins","nexus","tomcat-1","tomcat-2"]
+}
