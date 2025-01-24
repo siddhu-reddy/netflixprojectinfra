0d75d9bf9908ec161532fa3af9b1d50c9ad6c171
siddhu-reddy <121557022+siddhu-reddy@users.noreply.github.com>
2025-01-24 13:21:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one"{
   ami="ami-043a5a82b6cf98947"
   instance_type="t2.medium"
   key_name="sid"
-  vpc_security_groups_ids=["sg-003550d9d3300d12f"]
+  vpc_security_group_ids=["sg-003550d9d3300d12f"]
   tags={
   name=var.instance_name[count.index]
   }
